22ae685f99b1477f563cbd008f276150b860a6d0
Annvka <D273734@edu.curio.nl>
2025-03-17 09:30:55 +0100

    Html voor de nav opgezet

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepagina - Takenlijst</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="public_html/img/logo-big-v2.png">
+            <a href="">Takenlijst</a>
+            <a href="">Inloggen</a>
+        </nav>
+    </header>
+    
+</body>
+</html>
