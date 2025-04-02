983c9031f2807b5237f905746df0f0657a67a27c
Annvka <D273734@edu.curio.nl>
2025-04-02 14:57:36 +0200

    Voorbeeld taak structuur

--- tasks/index.php
+++ tasks/index.php
@@ -9,6 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Takenlijst</title>
     <link rel="stylesheet" href="../public_html/css/main.css">
+    <script src="https://kit.fontawesome.com/1b7f40349a.js" crossorigin="anonymous"></script>
 </head>
 <body>
     
@@ -28,20 +29,45 @@ session_start();
                 <h2>To do</h2>
             
                 <div class="tasks">
-                    <p class="taskName">Taaknaam</p>
+                    <div class="taskAlign">
+                        <p class="taskName">Taaknaam</p>
+                        <a href="" class="edit"><i class="fa-solid fa-pen-to-square"></i></a>
+                    </div>
+                    
                     <p class="departmentName">Afdeling</p>
                     <p class="deadlineName">Deadline: 28-4-2025</p>
                 </div>
-
 
             </div>
 
             <div class="taskColumn">
                 <h2>Doing</h2>
+                            
+                <div class="tasks">
+                    <div class="taskAlign">
+                        <p class="taskName">Taaknaam</p>
+                        <a href="" class="edit"><i class="fa-solid fa-pen-to-square"></i></a>
+                    </div>
+                    
+                    <p class="departmentName">Afdeling</p>
+                    <p class="deadlineName">Deadline: 28-4-2025</p>
+                </div>
+
             </div>
 
             <div class="taskColumn">
                 <h2>Done</h2>
+                            
+                <div class="tasks">
+                    <div class="taskAlign">
+                        <p class="taskName">Taaknaam</p>
+                        <a href="" class="edit"><i class="fa-solid fa-pen-to-square"></i></a>
+                    </div>
+                    
+                    <p class="departmentName">Afdeling</p>
+                    <p class="deadlineName">Deadline: 28-4-2025</p>
+                </div>
+
             </div>
         </div>
 
